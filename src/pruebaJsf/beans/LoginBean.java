95fbb379781795469713233072c229ad8a5ab17c
Brayam Andrey Oliveros Rivera <50060283+AndreyRi13@users.noreply.github.com>
2020-11-18 18:31:18 -0500

    Add files via upload

---
+++ src/pruebaJsf/beans/LoginBean.java
@@ -0,0 +1,56 @@
+package pruebaJsf.beans;
+
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+
+import co.edu.ufps.sistemajpa.dao.UsuarioDao;
+import co.edu.ufps.sistemajpa.entidades.Usuario;
+import lombok.Data;
+
+@ManagedBean
+@Data
+public class LoginBean {
+	
+	@ManagedProperty(value = "#{messageBean}")
+	private MessageBean messageBean;
+	
+	@ManagedProperty(value = "#{sessionBean}")
+	private SessionBean sessionBean;
+	
+	private String usuario;
+	
+	private List<Usuario> usuarios;
+	
+	private String pass;
+	
+	public String ingresar() {
+		System.out.println("Ingresar Usuario " + usuario + " Pass " + pass);
+		if(this.usuario.contentEquals("carlos")) {
+		
+			messageBean.setMessage("Bienvenido El usuario es " + usuario);
+			sessionBean.setUsuario(usuario);
+			return "exito";
+			
+		}else {
+			messageBean.setMessage("Error El usuario " + usuario + " es invalido" );
+			sessionBean.setUsuario(null);
+			return "error";
+			
+		}
+		// 
+	}
+	
+	public void validar() {
+		System.out.println("Validar Usuario " + usuario + " Pass " + pass);
+	}
+	
+	public void obtenerUsuarios() {
+		UsuarioDao uDao = new UsuarioDao();
+		this.usuarios = uDao.list();
+	}
+	
+	
+	
+}
